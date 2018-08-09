22bb8aa6ec22783224b426aaff524143fdfe2c3c
zven21 <andzven@gmail.com>
2018-08-09 10:16:02 +0800

    Parse params `q` expr.

--- lib/turbo_html/form.ex
+++ lib/turbo_html/form.ex
@@ -10,17 +10,27 @@ defmodule Turbo.HTML.Form do
 
   ## Example
 
-      iex> conn = %{params: %{"name_like" => "a", "title_like" => "b"}}
-      iex> Turbo.HTML.Form.turbo_search_input
+      iex> conn = %{params: %{"q" => %{"name_like" => "a", "title_like" => "b"}}}
+      iex> Turbo.HTML.Form.turbo_search_input(conn, "name_like")
       {:safe, []}
 
   """
   @spec turbo_search_input(Map.t(), String.t(), Keyword.t()) :: any()
-  def turbo_search_input(conn, field, opts \\ []) do
-    value = Map.get(conn.params, field, "")
+  def turbo_search_input(conn, field, opts \\ []), do: do_turbo_search_input(conn, field, opts)
+
+  defp do_turbo_search_input(%{params: %{"q" => query_params}}, field, opts) do
+    value = Map.get(query_params, field, "")
 
     content_tag(:input, "",
       name: field, value: value,
+      class: Keyword.get(opts, :class),
+      placeholder: Keyword.get(opts, :placeholder)
+    )
+  end
+
+  defp do_turbo_search_input(_, field, opts) do
+    content_tag(:input, "",
+      name: field,
       class: Keyword.get(opts, :class),
       placeholder: Keyword.get(opts, :placeholder)
     )
