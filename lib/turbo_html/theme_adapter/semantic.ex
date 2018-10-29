4c47596d138576e2bdbc1446e1aea8e9c470b8c1
zven21 <andzven@gmail.com>
2018-10-29 17:38:46 +0800

    add semantic theme adapter.

--- lib/turbo_html/theme_adapter/semantic.ex
+++ lib/turbo_html/theme_adapter/semantic.ex
@@ -2,4 +2,36 @@ defmodule Turbo.HTML.ThemeAdapter.Semantic do
   @moduledoc """
   Semantic Theme.
   """
+
+  defmacro pagination_links(do: expression) do
+    quote do
+      Phoenix.HTML.Tag.content_tag :div, class: "ui pagination menu" do
+        unquote(expression)
+      end
+    end
+  end
+
+  defmacro page_link(url, :disabled, do: text) do
+    quote do
+      Phoenix.HTML.Link.link to: unquote(url), class: "disabled item" do
+        unquote(text)
+      end
+    end
+  end
+
+  defmacro page_link(url, :active, do: text) do
+    quote do
+      Phoenix.HTML.Link.link to: unquote(url), class: "active item" do
+        unquote(text)
+      end
+    end
+  end
+
+  defmacro page_link(url, do: text) do
+    quote do
+      Phoenix.HTML.Link.link to: unquote(url), class: "item" do
+        unquote(text)
+      end
+    end
+  end
 end
