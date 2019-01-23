2bc5263ebaeca00c673397232595c025d5ad82e6
zven21 <andzven@gmail.com>
2019-01-23 16:16:48 +0800

    update mix

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Turbo.HTML.MixProject do
   use Mix.Project
 
-  @version "0.1.4"
+  @version "0.2"
   @github "https://github.com/zven21/turbo_html"
 
   def project do
@@ -28,8 +28,8 @@ defmodule Turbo.HTML.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
-      {:phoenix_html, "~> 2.2"},
-      {:plug, "~> 1.1"}
+      {:phoenix_html, "~> 2.13"},
+      {:plug, "~> 1.7"}
     ]
   end
 
